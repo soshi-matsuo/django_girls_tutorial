02d708161444275fe4005a76ca6309668a8b20d5
soshi-matsuo <msoshi1202@gmail.com>
2019-06-02 21:50:47 +0900

    finish tutorial

--- blog_tutorial/views.py
+++ blog_tutorial/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()) \
-                        .order_by('published_date')
+                        .order_by('-published_date')
     return render(request, 'blog_tutorial/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
